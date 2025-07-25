699eebf4a71919f6819175ed2128c68e7c97a41c
JZ23-2 <jacksonzhang077@gmail.com>
2025-07-25 15:37:53 +0700

    feat: add auth provider for microsfot

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+import React from "react";
+import { signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+
+const Dashboard = () => {
+  const { data, status } = useSession();
+
+  return status === "authenticated" ? (
+    <div style={{ textAlign: "center" }}>
+      <h1>Dashboard</h1>
+      {data && (
+        <>
+          <div>{`Name : ${data.user?.name}`}</div>
+          <div>{`Email : ${data.user?.email}`}</div>
+        </>
+      )}
+      <button onClick={() => signOut({ callbackUrl: "/" })}>Log out</button>
+    </div>
+  ) : (
+    <Link href="/">Log in</Link>
+  );
+};
+
+export default Dashboard;
